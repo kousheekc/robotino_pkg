4dd5b41a49bb5f4f691df2043e6411b680a1a287
kousheekc <kousheekc@gmail.com>
2024-07-11 13:15:36 +0200

    added params and gripper control

--- rto_node/scripts/robotino_teleop.py
+++ rto_node/scripts/robotino_teleop.py
@@ -1,31 +1,8 @@
-#!/usr/bin/env python3
-
-# Author: Saurabh Borse(saurabh.borse@alumni.fh-aachen.de)
-
-#  MIT License
-#  Copyright (c) 2023 Saurabh Borse
-#  Permission is hereby granted, free of charge, to any person obtaining a copy
-#  of this software and associated documentation files (the "Software"), to deal
-#  in the Software without restriction, including without limitation the rights
-#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#  copies of the Software, and to permit persons to whom the Software is
-#  furnished to do so, subject to the following conditions:
-
-#  The above copyright notice and this permission notice shall be included in all
-#  copies or substantial portions of the Software.
-
-#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#  SOFTWARE.
-
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
+from rto_msgs.msg import DigitalReadings
 
 class Robotino3Teleop(Node):
 
@@ -35,22 +12,41 @@ class Robotino3Teleop(Node):
         # create subscription to joy topic
         self.subscription = self.create_subscription(Joy, 'joy', self.TeleopCallback, 10)
         
-        # create publisher to cmd_vel topic
-        self.publisher= self.create_publisher(Twist, 'cmd_vel_joy', 10)
+        # create publishers
+        self.cmd_vel_publisher= self.create_publisher(Twist, 'cmd_vel_joy', 10)
+        self.gripper_publisher= self.create_publisher(DigitalReadings, 'set_digital_values', 10)
         
         # Initialize parameters
         self.declare_parameter('forward_axis_scalling', 1.0)
         self.declare_parameter('angular_axis_scalling', 1.0)
         self.declare_parameter('deadzone', 0.1)
+        self.declare_parameter('x_axis', 3)
+        self.declare_parameter('y_axis', 2)
+        self.declare_parameter('w_axis', 0)
+        self.declare_parameter('grip_button', 4)
+        self.declare_parameter('drop_button', 5)
+
+        self.grip_state = False
         
     # callback function to publish data over cmd_vel topic based on joy_pad inputs
-    def TeleopCallback(self, data):
+    def TeleopCallback(self, data: Joy):
+        if (data.buttons[self.get_parameter('grip_button').value] == 1):
+            self.grip_state = True
+        if (data.buttons[self.get_parameter('drop_button').value] == 1):
+            self.grip_state = False
+
+        grip_msg = DigitalReadings()
+        grip_msg.header.stamp = self.get_clock().now().to_msg()
+        grip_msg.values = [False] * 8
+        grip_msg.values[7] = self.grip_state
+        self.gripper_publisher.publish(grip_msg)
+    
         f_scale = self.get_parameter('forward_axis_scalling').value
         z_scale = self.get_parameter('angular_axis_scalling').value
 
-        x = data.axes[4]*f_scale
-        y = data.axes[3]*f_scale
-        w = data.axes[0]*z_scale
+        x = data.axes[self.get_parameter('x_axis').value]*f_scale
+        y = data.axes[self.get_parameter('y_axis').value]*f_scale
+        w = data.axes[self.get_parameter('w_axis').value]*z_scale
 
         if (abs(x) < self.get_parameter('deadzone').value):
             x = 0.0
@@ -72,7 +68,7 @@ class Robotino3Teleop(Node):
         p_msg.angular.y = 0.0
         p_msg.angular.z = w
 
-        self.publisher.publish(p_msg)
+        self.cmd_vel_publisher.publish(p_msg)
 
 def main():
     rclpy.init()
